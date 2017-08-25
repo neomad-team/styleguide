1e1855688027ceddbe1e59acf8c7809f65866eaa
newick <erick@entre-quote.com>
2017-08-25 14:42:10 +0200

    gulp and postcss beginning

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss')
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+// gulp.task('css', function () {
+//     var postcss    = require('gulp-postcss');
+//     var sourcemaps = require('gulp-sourcemaps');
+
+//     return gulp.src('src/**/*.css')
+//         .pipe( sourcemaps.init() )
+//         .pipe( postcss([ require('cssnext') ]) )
+//         .pipe( sourcemaps.write('.') )
+//         .pipe( gulp.dest('build/') );
+// });
+
+gulp.task('css', function () {
+  return (
+    gulp.src('./source/styles/00-base/base.css')
+      .pipe(postcss([
+        require("postcss-import")(),
+        require("postcss-url")(),
+        require("postcss-cssnext")(),
+        require("postcss-browser-reporter")(),
+        require("postcss-reporter")(),
+      ]))
+      .pipe(gulp.dest('source/dest/'))
+  )
+})
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+  
+      browserSync.init({
+          server: "./source"
+      });
+  
+      gulp.watch('./source/styles/00-base/base.css', ['css']);
+      gulp.watch("./source/*.html").on('change', browserSync.reload);
+  });
+
+// Rerun the task when a file changes
+// gulp.task('watch', function() {
+
+//     browserSync.init({
+//       file: ['./source']
+//     });
+
+//     gulp.watch('./source/styles/00-base/base.css', ['css']);
+// });
