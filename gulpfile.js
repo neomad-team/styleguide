c3f6192717e8e2a15fb437b55d0c21913fa7023a
newick <erick@entre-quote.com>
2017-09-06 15:34:35 +0200

    a bit of cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -2,56 +2,31 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss')
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  // place code for your default task here
+// Static Server + watching css/html files
+gulp.task('serve', ['postcss'], function() {
+
+    browserSync.init({
+        server: "./source"
+    });
+
+    gulp.watch("./source/styles/**/*.css", ['postcss']);
+    gulp.watch("./source/*.html").on('change', browserSync.reload);
 });
 
-// gulp.task('css', function () {
-//     var postcss    = require('gulp-postcss');
-//     var sourcemaps = require('gulp-sourcemaps');
+// Compile postCSS
+gulp.task('postcss', function () {
+    return (
+        gulp.src('./source/styles/main.css')
+        .pipe(postcss([
+            require("postcss-import")(),
+            require("postcss-url")(),
+            require("postcss-cssnext")(),
+            require("postcss-browser-reporter")(),
+            require("postcss-reporter")(),
+        ]))
+        .pipe(gulp.dest('./source/dest'))
+    )
+});
 
-//     return gulp.src('src/**/*.css')
-//         .pipe( sourcemaps.init() )
-//         .pipe( postcss([ require('cssnext') ]) )
-//         .pipe( sourcemaps.write('.') )
-//         .pipe( gulp.dest('build/') );
-// });
 
-gulp.task('css', function () {
-  return (
-    gulp.src('./source/styles/00-base/base.css'),
-    gulp.src('./source/styles/04-templates/home.css')
-      .pipe(postcss([
-        require("postcss-import")(),
-        require("postcss-url")(),
-        require("postcss-cssnext")(),
-        require("postcss-browser-reporter")(),
-        require("postcss-reporter")(),
-      ]))
-      .pipe(gulp.dest('source/dest/'))
-  )
-})
-
-// Static Server + watching scss/html files
-gulp.task('serve', function() {
-
-      browserSync.init({
-          server: "./source"
-      });
-
-      gulp.watch('./source/styles/00-base/base.css', ['css']);
-      gulp.watch('./source/styles/**/*.css', ['css']);
-      gulp.src('./source/styles/04-templates/home.css')
-      gulp.watch("./source/styles/**/*.css").on('change', browserSync.reload);
-      gulp.watch("./source/*.html").on('change', browserSync.reload);
-  });
-
-// Rerun the task when a file changes
-// gulp.task('watch', function() {
-
-//     browserSync.init({
-//       file: ['./source']
-//     });
-
-//     gulp.watch('./source/styles/00-base/base.css', ['css']);
-// });
+gulp.task('default', ['serve']);
