e487f555d4971bd7c9d2b4867c91fc57e3c18f3c
fuentesloic <fuentes.loic@gmail.com>
2018-08-16 08:00:05 -0400

    fix duplicate value in css build adding discard duplicate module and add linter during builds

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,31 @@
 module.exports = (ctx) => ({
 
   plugins: {
+    'stylelint': {
+      'extends': './postcss.linter.json',
+      'rules': {
+        'at-rule-empty-line-before': null
+      }
+    },
     'precss': {},
+    'postcss-pxtorem': {},
+    'postcss-custom-properties': {
+      preserve: ctx.env === 'dev'
+    },
     'postcss-preset-env': {
       stage: 2,
       features: {
-        'nesting-rules': true
+        'nesting-rules': true,
+        'color-mod-function': {
+          unresolved: 'warn'
+        }
       },
       browsers: 'defaults',
       autoprefixer: {
         add: ctx.env === 'prod'
       }
     },
+    'postcss-discard-duplicates': {},
     'cssnano': ctx.env === 'prod'
   }
 })
